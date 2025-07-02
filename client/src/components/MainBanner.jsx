665a44d2a45ab1c1279915b8905dbc0a4072c371
Methma-02 <146626620+Methma-02@users.noreply.github.com>
2025-07-02 10:09:10 +0530

    .

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { Link} from 'react-router-dom'
 
 function MainBanner() {
   return (
     <div className='relative'>
       <img src={assets.main_banner_bg} alt="banner" className='w-full hidden md:block' />
       <img src={assets.main_banner_bg_sm} alt="banner" className='w-full md:hidden' />
+      <div>
+        <h1>Freshness You Can Trust, Savings You Will Love!</h1>
+      </div>
+      <div>
+        <Link to={"/products"} className='group flex items-center gap-2 px-7 md:px-9 py-3 bg-primary hover:bg-primary-dull transition rounded text-white cursor-pointer'>Shop now
+        <img className='md:hidden transition group-focus:translate-x-1' src={assets.white_arrow_icon} alt="arrow" />
+        </Link>
+        <Link to={"/products"} className='group flex items-center gap-2 px-7 md:px-9 py-3 bg-primary hover:bg-primary-dull transition rounded text-white cursor-pointer'>
+        Explore Deals
+        <img className='md:hidden transition group-focus:translate-x-1' src={assets.white_arrow_icon} alt="arrow" />
+        </Link>
+      </div>
     </div>
   )
 }
